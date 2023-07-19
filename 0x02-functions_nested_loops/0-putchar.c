597e13f5e671166de521534f834cefb69021feda
arwa <saih.arwa.mcn@gmail.com>
2023-07-19 10:49:53 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/** 
+
+  * main - the Entry point 
+  * 
+  * Return : Always 0 (success)
+  
+  */
+
+int main(void)
+
+{
+ printf("_putchar\n ");
+ return (0);
+}
