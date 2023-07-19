8c597f1c272617a5c7454fe958ff156f728983d3
arwa <saih.arwa.mcn@gmail.com>
2023-07-19 13:56:01 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,16 +7,16 @@
 */
 int main(void)
 {
-    char dev[] = "_putchar"; 
-    int x = 0;
+char dev[] = "_putchar"; 
+int x = 0;
 
-    while (dev[x] != '\0') 
-    {
-        _putchar(dev[x]); 
-        x++;
-    }
+while (dev[x] != '\0') 
+{
+_putchar(dev[x]); 
+x++;
+}
 
-    _putchar('\n');
-    return (0);
+_putchar('\n');
+return (0);
 }
 
