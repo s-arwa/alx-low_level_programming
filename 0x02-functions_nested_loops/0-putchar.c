a6760ce7e376d2a1b2a0ddaefffd2c8dc6549fb8
arwa <saih.arwa.mcn@gmail.com>
2023-07-19 17:41:59 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - Prints "Holberton" followed by a new line.
- *
- * Return: Always 0.
- */
+* main - main block
+* Description: Print _putchar followed by a new line.
+* Return: 0
+*/
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+char alx[] = "_putchar";
+int i = 0;
 
-	return (0);
+while (alx[i] != '\0')
+{
+_putchar(alx[i]);
+i++;
+}
+
+_putchar('\n');
+return (0);
 }
