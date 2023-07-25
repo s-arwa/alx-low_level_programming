42d1ab54820dc59ba8ad9f2ec22455ae19e9c0de
arwa <saih.arwa.mcn@gmail.com>
2023-07-24 20:36:31 -0700

    do

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes apointer to an int as parameter
+ * and update the value of it to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,9 +9,9 @@ void print_array(int *a, int n)
 {
 int i;
 	
-for (i = 0 ; i < n; i++)
+for (i = 0; i < n; i++)
 {
-if (i != (n - 1))
+if (i == 0; i != (n - 1))
 printf("%d", a[i]);
 else
 printf("%d", a[i]);
